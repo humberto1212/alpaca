3b9d548214d1875d1a94ddb82382ef9872df02ab
humberto1212 <angeluccirincon@gmail.com>
2025-03-28 07:28:45 +0100

    feat(middleware): Add CORS middleware for handling cross-origin requests

--- alpaca/middleware.go
+++ alpaca/middleware.go
@@ -3,6 +3,7 @@ package alpaca
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type Middleware func(Handler) Handler
@@ -49,5 +50,30 @@ func RecoveryMiddleware(next Handler) Handler {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Apply Recovery Middleware")
 		next(w, r)
+	}
+}
+
+func EnableCors(router *Router) Middleware {
+	return func(next Handler) Handler {
+		return func(w http.ResponseWriter, r *http.Request) {
+
+			config := router.getCors()
+
+			fmt.Println(strings.Join(config.allowAllOrigins, ", "))
+
+			w.Header().Set("Access-Control-Allow-Origin", strings.Join(config.allowAllOrigins, ", "))
+			w.Header().Set("Access-Control-Allow-Methods", strings.Join(config.allowMethods, ", "))
+			w.Header().Set("Access-Control-Allow-Headers", strings.Join(config.allowHeaders, ", "))
+			w.Header().Set("Access-Control-Expose-Headers", strings.Join(config.exposeHeaders, ", "))
+			w.Header().Set("Access-Control-Allow-Credentials", fmt.Sprintf("%t", config.allowCredentials))
+			w.Header().Set("Access-Control-Max-Age", fmt.Sprintf("%d", config.maxAge))
+
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusNoContent)
+				return
+			}
+
+			next(w, r)
+		}
 	}
 }
